dfb9596c024cdaf9cdd1ffa4d58da3782946cac3
Md Zameer <165930608+mdzameercse@users.noreply.github.com>
2025-07-16 00:36:00 +0530

    Update CorsConfig.java

--- ChatApp/src/main/java/com/zameer/ChatApp/Config/CorsConfig.java
+++ ChatApp/src/main/java/com/zameer/ChatApp/Config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // All APIs
-                        .allowedOrigins("http://localhost:3000") // Allow React app
+                         .allowedOrigins("https://your-netlify-site.netlify.app")
                         .allowedMethods("*") // GET, POST, PUT, DELETE
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies/auth headers
